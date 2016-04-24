84e7ce236351511ad0146871facde42deb10a5e9
PrithuGit <prithviprakashr@gmail.com>
2016-04-24 10:58:03 +0530

    Added all pages of form

--- src/com/_4bTrucking/AgreementEditor.java
+++ src/com/_4bTrucking/AgreementEditor.java
@@ -48,14 +48,18 @@ public class AgreementEditor {
 		Map<String, String> fieldNameAndValue = gson.fromJson(data, type);
 		String agreementType = fieldNameAndValue.get("agreementType");
 		System.out.println(fieldNameAndValue);
-		String src = "G:/Softwares/webserver/apache-tomcat-8.0.27/apache-tomcat-8.0.27/webapps/4bTruckingAgreementSave/"
+		/*String src = "G:/Softwares/webserver/apache-tomcat-8.0.27/apache-tomcat-8.0.27/webapps/4bTruckingAgreementSave/"
 				+ agreementType + "Src.pdf";
 		String test = "G:/Softwares/webserver/apache-tomcat-8.0.27/apache-tomcat-8.0.27/webapps/4bTruckingAgreementSave/"
-				+ agreementType + ".pdf";
+				+ agreementType + ".pdf";*/
 		/*String src = "/data/apache-tomcat-7.0.35/webapps/4bTruckingAgreementSave/"
 				+ agreementType + "Src.pdf";
 		String test = "/data/apache-tomcat-7.0.35/webapps/4bTruckingAgreementSave/"
 				+ agreementType + ".pdf";*/
+		String src = "/data/tomcat8/webapps/4bTruckingAgreementSave/"
+		+ agreementType + "Src.pdf";
+String test = "/data/tomcat8/webapps/4bTruckingAgreementSave/"
+		+ agreementType + ".pdf";
 		try {
 			File target = new File(test);
 			File pdf = new File(src);
